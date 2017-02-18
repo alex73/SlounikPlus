79d7be3303e635a93b000517f28964e4c59be846
Aleś Bułojčyk <alex73mail@gmail.com>
2017-02-18 14:04:03 +0300

    Старт кліента

--- client/src/org/im/dc/client/ui/ClientStartup.java
+++ client/src/org/im/dc/client/ui/ClientStartup.java
@@ -1,29 +1,7 @@
 package org.im.dc.client.ui;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
-import javax.swing.JOptionPane;
-
-import org.im.dc.client.WS;
-import org.im.dc.service.dto.Article;
-
 public class ClientStartup {
-
-    public static void main(String[] args) {
-        try {
-            Thread.sleep(1000);
-            System.out.println("start");
-            WS.init("http://localhost:9080/myapp/articles?wsdl", "u", "p");
-
-            long be = System.currentTimeMillis();
-            ordered();
-            long af = System.currentTimeMillis();
-            System.out.println(af - be);
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Error start client: " + ex.getMessage());
-        }
+    public static void main(String[] args) throws Exception {
+        new MainController().start();
     }
 }
--- client/src/org/im/dc/client/ui/ClientStartup2.java
+++
@@ -1,7 +0,0 @@
-package org.im.dc.client.ui;
-
-public class ClientStartup2 {
-    public static void main(String[] args) throws Exception {
-        new MainController().start();
-    }
-}
