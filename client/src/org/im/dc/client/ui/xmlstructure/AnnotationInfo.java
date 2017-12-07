3d1b11b8c5a5abd035b0dc35fb2ba4b42cf51057
Aleś Bułojčyk <alex73mail@gmail.com>
2017-12-07 08:28:33 +0300

    New annotations support

--- client/src/org/im/dc/client/ui/xmlstructure/AnnotationInfo.java
+++ client/src/org/im/dc/client/ui/xmlstructure/AnnotationInfo.java
@@ -7,16 +7,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.xerces.xs.XSAnnotation;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class AnnotationInfo {
-    private static final Pattern RE_DETAILS = Pattern.compile("\\{.+?\\}");
     private static final Pattern RE_COLOR = Pattern.compile("#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})");
 
     enum EDIT_TYPE {
         DEFAULT, COMBO, COMBO_EDITABLE, RADIO, CHECK, CUSTOM, ARTICLES_LIST
     };
 
-    public String text;
+    public String text, tooltip;
     public Color fgColor, bgColor, borderColor;
     public EDIT_TYPE editType = EDIT_TYPE.DEFAULT;
     public String editDetails;
@@ -24,28 +27,16 @@ public class AnnotationInfo {
 
     public AnnotationInfo(XSAnnotation ann, String tagName) {
         if (ann != null) {
-            String t = ann.getAnnotationString().replaceAll("<[^<]+>", "").trim();
-            Matcher m = RE_DETAILS.matcher(t);
-            while (m.find()) {
-                processSetting(m.group(0));
-            }
-            text = m.replaceAll("");
+            ann.writeAnnotation(parser, XSAnnotation.SAX_CONTENTHANDLER);
         }
         if (text == null || text.trim().isEmpty()) {
             text = tagName.replace('_', ' ');
         }
     }
 
-    private void processSetting(String s) {
-        String cn = s.substring(1, s.length() - 1);
-        int p = cn.indexOf(':');
-        String key, value;
-        if (p < 0) {
-            key = cn;
-            value = null;
-        } else {
-            key = cn.substring(0, p);
-            value = cn.substring(p + 1);
+    private void processSetting(String key, String value) {
+        if (key == null) {
+            throw new RuntimeException("Wrong annotation without key: " + value);
         }
         switch (key) {
         case "fg":
@@ -67,15 +58,15 @@ public class AnnotationInfo {
             RORW r = new RORW();
             enables.add(r);
             r.writable = "rw".equals(key);
-            p = value.indexOf('/');
+            int p = value.indexOf('/');
             if (p <= 0) {
-                throw new RuntimeException("Wrong annotation: " + cn);
+                throw new RuntimeException("Wrong annotation: " + value);
             }
             r.role = value.substring(0, p);
             r.state = value.substring(p + 1);
             break;
         default:
-            throw new RuntimeException("Wrong annotation: " + cn);
+            throw new RuntimeException("Wrong annotation: " + key + ":" + value);
         }
     }
 
@@ -88,6 +79,58 @@ public class AnnotationInfo {
         return new Color(Integer.parseInt(m.group(1), 16), Integer.parseInt(m.group(2), 16),
                 Integer.parseInt(m.group(3), 16));
     }
+
+    ContentHandler parser = new DefaultHandler() {
+        String attrName;
+        StringBuilder str = new StringBuilder();
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes)
+                throws SAXException {
+            attrName = null;
+            str.setLength(0);
+            switch (localName) {
+            case "appinfo":
+                for (int i = 0; i < attributes.getLength(); i++) {
+                    switch (attributes.getLocalName(i)) {
+                    case "attr":
+                        attrName = attributes.getValue(i);
+                        break;
+                    }
+                }
+                break;
+            case "documentation":
+                for (int i = 0; i < attributes.getLength(); i++) {
+                    switch (attributes.getLocalName(i)) {
+                    case "type":
+                        attrName = attributes.getValue(i);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            str.append(ch, start, length);
+        }
+
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            switch (localName) {
+            case "appinfo":
+                processSetting(attrName, str.toString());
+                break;
+            case "documentation":
+                if ("tooltip".equals(attrName)) {
+                    tooltip = str.toString();
+                } else if (attrName == null) {
+                    text = str.toString();
+                }
+                break;
+            }
+            str.setLength(0);
+        }
+    };
 
     public static class RORW {
         boolean writable;
