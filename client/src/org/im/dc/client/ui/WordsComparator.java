b58df16aaf8d4c1f39b4ceef5032253551994b6d
Aleś Bułojčyk <alex73mail@gmail.com>
2019-01-02 20:47:13 +0300

    Sort articles

--- client/src/org/im/dc/client/ui/WordsComparator.java
+++ client/src/org/im/dc/client/ui/WordsComparator.java
@@ -1,15 +1,19 @@
 package org.im.dc.client.ui;
 
+import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 
 public class WordsComparator implements Comparator<String> {
+    public static final Locale BE_LOC = new Locale("be");
+    public static final Collator BE = Collator.getInstance(BE_LOC);
     public static final WordsComparator INSTANCE = new WordsComparator();
 
     @Override
     public int compare(String o1, String o2) {
         String s1 = charsOnly(o1);
         String s2 = charsOnly(o2);
-        int r = s1.compareToIgnoreCase(s2);
+        int r = BE.compare(s1.toLowerCase(BE_LOC), s2.toLowerCase(BE_LOC));
         if (r == 0) {
             r = o1.toString().compareTo(o2.toString());
         }
